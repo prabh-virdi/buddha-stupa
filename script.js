f854c02ee5f8d6130473981684c47b1905119351
prabh-virdi <rajavirdi2001@gmail.com>
2024-06-27 11:51:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -62,7 +62,8 @@ document.addEventListener('DOMContentLoaded', () => {
         const arrow = document.createElement('div');
         arrow.classList.add('arrow', direction);
         const img = document.createElement('img');
-        img.src = imageSrc; // Set custom image source for arrows
+        img.src = 'images/left.png'; // Set custom image source for arrows
+        img.src = 'images/right.png';
         arrow.appendChild(img);
         arrow.addEventListener('click', () => {
             gallery.scrollBy({
